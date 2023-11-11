1bc7161ba383b1fb847c6fbacb347d8063546a66
yunyoung1819 <yunyoung1819@hanmail.net>
2023-11-11 23:38:33 +0900

    feat: Named Lock 적용하기

---
+++ src/main/java/com/example/stock/facade/NamedLockStockFacade.java
@@ -0,0 +1,30 @@
+package com.example.stock.facade;
+
+import com.example.stock.repository.LockRepository;
+import com.example.stock.service.StockService;
+import org.springframework.stereotype.Component;
+
+import javax.transaction.Transactional;
+
+@Component
+public class NamedLockStockFacade {
+
+	private final LockRepository lockRepository;
+
+	private final StockService stockService;
+
+	public NamedLockStockFacade(LockRepository lockRepository, StockService stockService) {
+		this.lockRepository = lockRepository;
+		this.stockService = stockService;
+	}
+
+	@Transactional
+	public void decrease(Long id, Long quantity) {
+		try {
+			lockRepository.getLock(id.toString()); // 락을 획득
+			stockService.decrease(id, quantity);
+		} finally {
+			lockRepository.releaseLock(id.toString());  // 락을 해제
+		}
+	}
+}
--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -3,8 +3,9 @@ package com.example.stock.service;
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 
 @Service
 public class StockService {
@@ -15,8 +16,8 @@ public class StockService {
 		this.stockRepository = stockRepository;
 	}
 
-//	@Transactional
-	public synchronized void decrease(Long id, Long quantity) {
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
+	public void decrease(Long id, Long quantity) {
 		// Stock 조회
 		// 재고를 감소한 뒤
 		// 갱신된 값을 저장
